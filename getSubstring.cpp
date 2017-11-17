3901e57185bc511a3a1ed721ca2d2c05130c9338
Andrey Khodko <khodand@outlook.com>
2017-11-17 16:19:07 +0300

    Create getSubstring.cpp

---
+++ getSubstring.cpp
@@ -0,0 +1,16 @@
+#include "String.h"
+
+// [first, last)
+String* getSubstring(String &string, int first, int last) {
+	int newSize = last - first;
+	char* subData = new char[newSize];
+
+	for (int i = first; i < last; ++i) 
+		subData[i - first] = string.data[i];
+
+	String *result = new String;
+	result->data = subData;
+	result->size = newSize;
+
+	return result;
+}
