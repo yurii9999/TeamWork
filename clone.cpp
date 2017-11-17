16b6f1520316825d8731eb2dbf7b2b1ff59eadd3
yurii9999 <33332442+yurii9999@users.noreply.github.com>
2017-11-17 16:07:03 +0300

    Create clone.cpp

---
+++ clone.cpp
@@ -0,0 +1,13 @@
+String* clone(String &string)
+{
+    String* newString = new String;
+    newString->size = string.size;
+    newString->data = new char[newString->size];
+
+    for (int i = 0; i < newString->size; i++)
+    {
+        newString->data[i] = string.data[i];
+    }
+
+    return newString;
+}
