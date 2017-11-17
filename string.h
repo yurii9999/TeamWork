7ff48912d24bc19edd8c137467eb3acba6be1325
Degiv <32798005+Degiv@users.noreply.github.com>
2017-11-17 15:37:04 +0300

    Update string.h

--- string.h
+++ string.h
@@ -7,3 +7,17 @@ struct String
     char *data = new char[firstSize];
     int size = firstSize;
 };
+
+bool areEqual(String &string1, String &string2);
+
+void input(String &string);
+
+void output(String &string);
+
+void clone(String &string);
+
+void cont(String &string);
+
+String* getSubstring(String &string, int first, int last);
+
+void clear(String &string);
