1a2a6c5984543954a49b4013c2352d5f63ffe75d
Andrey Khodko <khodand@outlook.com>
2017-11-17 16:31:07 +0300

    Update string.h

--- string.h
+++ string.h
@@ -11,10 +11,10 @@ bool areEqual(String &string1, String &string2);
 void input(String &string);
 void output(String &string);
 
-String* clone(String &string);
+String clone(String &string);
 
 void cont(String &string, String &addition);
-String* getSubstring(String &string, int first, int last);
+String getSubstring(String &string, int first, int last);
 
 void clear(String &string);
 
