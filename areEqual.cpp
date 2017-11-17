91fb6af474af1854e5d7dee56b6074694e28f900
Andrey Khodko <khodand@outlook.com>
2017-11-17 16:27:15 +0300

    Update areEqual.cpp

--- areEqual.cpp
+++ areEqual.cpp
@@ -3,13 +3,11 @@
 
 using namespace std;
 
-bool areEqual(String &string1, String &string2)
-{
+bool areEqual(String &string1, String &string2) {
     if (string1.size != string2.size)
         return false;
 
-    for (int i = 0; i < string1.size; ++i)
-    {
+    for (int i = 0; i < string1.size; ++i) {
         if (string1.data[i] != string2.data[i])
             return false;
     }
